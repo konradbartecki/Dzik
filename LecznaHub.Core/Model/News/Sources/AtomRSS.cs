bd279020d1777dd74336d62fce130d2bfc1fae79
Konrad Bartecki <konradbartecki@outlook.com>
2015-08-31 04:12:21 +0200

    Adding AtomRSS data source implementation

---
+++ LecznaHub.Core/Model/News/Sources/AtomRSS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using LecznaHub.Core.Model.Common;
+using LecznaHub.Core.Providers;
+
+namespace LecznaHub.Core.Model.News.Sources
+{
+    public static class AtomRSS
+    {
+        public static async Task<UniversalNewsCollection> GetRssFeedAsync(string feedurl)
+        {
+            //Download RSS feed
+            var request = new Request(feedurl);
+            var data = await Downloader.DownloadWebStringAsync(request);
+
+            XDocument newsXmlDocument = XDocument.Parse(data);
+            var XmlItems = newsXmlDocument.Descendants("item");
+            //Cast items into model
+            UniversalNewsCollection collection = new UniversalNewsCollection();
+            foreach (XElement item in XmlItems)
+            {
+                string id = item.Element("link").Value;
+                string title = item.Element("title").Value;
+                string description = item.Element("description").Value;
+
+                collection.Items.Add(new UniversalNewsItem
+                {
+                    UniqueId = id,
+                    Title = title,
+                    Description = description
+                });
+            }
+            return collection;
+        }
+    }
+}
